0109fca52b291638fe50fe2991e3cd34fbf733ba
Yuri JI <45191069+ur2e@users.noreply.github.com>
2022-07-02 22:34:42 +0900

    0702

--- src/com/company/baekjoon/Main.java
+++ src/com/company/baekjoon/Main.java
@@ -1,7 +1,27 @@
 package com.company.baekjoon;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int start = 1;
+        int day, night, answer=1;
+        int nextDay = a-b;
+        while(start < v) {
+            start += a;
+            answer++;
+            if(start >= v) break;
+            start -= b;
+
+        }
+        System.out.println(answer);
     }
 }
