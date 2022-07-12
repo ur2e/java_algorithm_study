9989144e950bcef00692a69b446211873904c77e
Yuri JI <45191069+ur2e@users.noreply.github.com>
2022-07-12 19:08:05 +0900

    0712

---
+++ src/com/company/baekjoon/DFS_BFS/A에서B_16953.java
@@ -0,0 +1,35 @@
+package com.company.baekjoon.DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A에서B_16953 {
+    static long a, b;
+    static int answer = -1;
+    static void bfs(long n, int count){
+        if(answer != -1) return;
+        if(n == b) {
+            answer = count;
+            return;
+        }
+        else if(n > b)   return;
+        else{
+            //System.out.println(count + " " + n);
+            bfs(n*2, count+1);
+            long tmp = (n*10) + 1;
+            bfs(tmp, count+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+
+        bfs(a, 1);
+
+        System.out.println(answer);
+    }
+}
--- src/com/company/baekjoon/마인크래프트.java
+++ src/com/company/baekjoon/마인크래프트.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 마인크래프트 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
