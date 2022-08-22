e3767162f2390ee587fa1578d93cd1cd87cb6848
Yuri JI <45191069+ur2e@users.noreply.github.com>
2022-08-22 23:09:13 +0900

    0822

--- src/com/company/java_algorithm_study/Section3_Twopointers/연속부분수열_다시보기/Me.java
+++ src/com/company/java_algorithm_study/Section3_Twopointers/연속부분수열_다시보기/Me.java
@@ -3,36 +3,29 @@ package com.company.java_algorithm_study.Section3_Twopointers.연속부분수열
 import java.util.Scanner;
 
 public class Me {
-    private int solution(int n, int m, int[] arr){
-        int answer = 0;
-
-        for(int i = 0; i < n; i++){
-            int p1 = i, p2 = p1+1;
-            int sum = arr[i];
-            while(p2 < n) {
-                if(sum < m) sum += arr[p2++];
-                else if (sum == m)  {
-                    answer++;
-                    break;
-                }
-                else break;
-            }
-        }
-
-        return answer;
-    }
     public static void main(String[] args){
-        Me T = new Me();
-        Scanner sc = new Scanner(System.in);
-
+        Scanner sc=new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        sc.close();
-        System.out.println(T.solution(n ,m, arr));
+
+        int answer = 0;
+        int sum = 0;
+        int lt = 0;
+        for(int rt = 0; rt <n ; rt++){
+            sum += arr[rt];
+            if(sum == m) {
+                answer++;
+                sum -= arr[lt++]; // 어짜피 rt가 계속증가해서 커지니까 이 문장 없어도 된다.
+            } else if(sum > m) {
+                while(sum > m){
+                    sum -= arr[lt++];
+                }
+            }
+        }
+        System.out.println(answer);
     }
 }
