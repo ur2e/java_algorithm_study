f4c07c062b7dffe10aeec217346020f247b66346
Yuri JI <45191069+ur2e@users.noreply.github.com>
2023-03-19 16:20:12 +0900

    [unrated] Title: 무인도 여행, Time: 2.87 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
@@ -1,56 +1,86 @@
 import java.util.*;
-class Solution {
-    static List<Integer> answer = new ArrayList<>();
-    static int width;
-    static int height;
-    static char[][] map ;// 지도
-    static int[][] visited ;//방문여부 저장
-    static int[] xdir={0,0,-1,1};
-    static int[] ydir={-1,1,0,0};
-    
-    int bfs(int x, int y){
-        int temp=map[x][y]-'0';
-        for(int k=0;k<4;k++){//상하좌우 4방향
-            int dx=x+xdir[k];
-            int dy=y+ydir[k];
-            if(dx >= 0 && dx < height && dy >= 0 && dy < width){
-                if(visited[dx][dy] == 0 && map[dx][dy] != 'X'){
-                    visited[dx][dy]=1;
-                    temp+=bfs(dx,dy);    
-                }
-            } 
-        }
-        return temp;
-    }
-    
-    public int[] solution(String[] maps) {
-        width=maps[0].length();
-        height=maps.length;
-        map = new char[maps.length][];
-        visited = new int[maps.length][maps[0].length()];
-        
-        //지도 초기화
-        for(int i=0;i<maps.length;i++){
-            map[i]=maps[i].toCharArray();
-        }
 
-        //지도에서 방문하지 않은 무인도를 방문 
-         for(int i=0;i<maps.length;i++){
-            for(int j=0;j<map[i].length;j++){
-                if(map[i][j] != 'X' && visited[i][j]==0){
-                    visited[i][j]=1;
-                    int isLand=bfs(i,j);
-                    if (isLand != 0) {
-                        answer.add(isLand);
-                    }
+class Point {
+    int x;
+    int y;
+    
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Solution {
+    int[] dx = {-1, 0, 1 ,0};
+    int[] dy = {0, 1, 0, -1};
+    
+    int n, m;
+    
+    static int[][] map;
+    Queue<Point> q = new LinkedList<>();
+    
+    public int BFS(int x, int y) {
+        q.offer(new Point(x, y));
+        int sum = map[x][y];
+        map[x][y] = 0;
+        
+        while(!q.isEmpty()){
+            Point p = q.poll();
+            
+            for(int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] > 0){
+                    q.offer(new Point(nx, ny));
+                    sum += map[nx][ny];
+                    map[nx][ny] = 0;
                 }
             }
         }
-        Collections.sort(answer);
         
-        if(answer.isEmpty()){
-            answer.add(-1);
+        return sum;
+    }
+    
+    public int[] solution(String[] maps) {
+        n = maps.length;
+        m = maps[0].length();
+        
+        map = new int[n][m];
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        
+        for(int i = 0; i < n; i++) {
+            // split 때문에 나는 런타임 에러도 아니었다.
+            char[] tmp = maps[i].trim().toCharArray();
+            for(int j = 0; j < m; j++) {
+                if(tmp[j] == 'X') map[i][j] = 0;
+                else { 
+                    map[i][j] = tmp[j] - '0'; 
+                }
+            }
         }
-        return answer.stream().mapToInt(i -> i).toArray();
+        // for(int i = 0; i < n; i++) {
+        //     // split 때문에 나는 런타임 에러 아님
+        //     char[] tmp = maps[i].toCharArray();
+        //     for(int j = 0; j < m; j++) {
+        //         if(tmp[j] == 'X') map[i][j] = 0;
+        //         else { // 2. Integer.parseInt도 아님
+        //             map[i][j] = Character.getNumericValue(tmp[j]); 
+        //         }
+        //     }
+        // }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(map[i][j] > 0)  {
+                    ans.add(BFS(i, j));
+                }
+            }
+        }
+        
+        Collections.sort(ans);
+        return ans.isEmpty()? new int[]{-1} : ans.stream().mapToInt(i -> i).toArray();
     }
 }
