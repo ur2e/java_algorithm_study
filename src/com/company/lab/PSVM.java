358994dd45a97ad935fb51135407e966ca961790
yuri.ji <dbfl9711@gmail.com>
2023-03-26 22:27:03 +0900

    자바의 정석

---
+++ src/com/company/lab/PSVM.java
@@ -0,0 +1,31 @@
+package com.company.lab;
+
+class Tv {
+    boolean power; // 전원 상태 (on/off)
+    int channel;   // 채널
+
+    void power() { power = !power; }
+    void channelUp() { ++channel; }
+    void channelDown() { --channel; }
+}
+
+class CaptionTv extends Tv {
+    boolean caption; // 캡션 상태 (on/off)
+    void displayCaption(String text) {
+        if (caption) {   // 캡션 상태가 on 일때만 text를 보여준다.
+            System.out.println(text);
+        }
+    }
+}
+
+class CaptionTvTest{
+    public static void main(String[] args) {
+        CaptionTv ctv = new CaptionTv();
+        ctv.channel = 10; // 조상 클래스로부터 상속받은 멤버
+        ctv.channelUp();
+        System.out.println(ctv.channel);
+        ctv.displayCaption("자막 자막 자막");
+        ctv.caption = true; //  자막 기능 켜기
+        ctv.displayCaption("자막 자막 자막");
+    }
+}
