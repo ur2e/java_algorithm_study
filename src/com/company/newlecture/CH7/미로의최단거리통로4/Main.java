64c2ca99b45c0cbf365f051cdefbacb7b37ec328
yuri.ji <dbfl9711@gmail.com>
2023-03-30 02:49:27 +0900

    레벨 탐색, bfs 기본 문제

--- src/com/company/newlecture/CH7/미로의최단거리통로4/Main.java
+++ src/com/company/newlecture/CH7/미로의최단거리통로4/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     static int[][] visited;
@@ -83,5 +82,4 @@ public class Main {
 1101000
 1000100
 1010100
-
  */
