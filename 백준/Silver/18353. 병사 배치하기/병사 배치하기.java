bf3e0370bb044a68d732f1160275351c0ba97dd0
Yuri JI <45191069+ur2e@users.noreply.github.com>
2023-04-04 21:15:00 +0900

    [Silver II] Title: 병사 배치하기, Time: 156 ms, Memory: 14708 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -18,53 +18,16 @@ public class Main {
 
         int max = 0;
         for (int i = 1; i <= n; i++) {
-            int tmpMax = 0;
             for(int j = i-1; j >= 0; j--){
                 if (power[i] < power[j]){
-                    tmpMax = Math.max(tmpMax, d[j]);
+                    d[i] = Math.max(d[i], d[j]+1);
                 }
             }
-            d[i] = tmpMax + 1;
             max = Math.max(max, d[i]);
         }
 
-        int answer = n - max;
+        int answer = n - (max+1);
         System.out.println(answer);
-
-//        for (int i = 1; i <= n; i++) {
-//            //System.out.println("-------");
-//
-//            //System.out.println("현재 병사는 ? : " + i);
-//            //System.out.println(power[i-1] + " " + power[i]);
-//            // 전투력 내림차순
-//            if(power[i-1] > power[i]) {
-//                d[i] = d[i-1] + power[i];
-//                //System.out.println("내림차순: " + d[i]);
-//            }
-//
-//            // 전투력 오름차순
-//            else {
-//                // 자신보다 전투력 센 병사 찾기
-//                int idx = i-1;
-//                int cnt = 0;
-//                while(power[idx] < power[i]){
-//                    //System.out.println("이 병사 제낀다. " + power[i] + " " + power[idx]);
-//                    idx--;
-//                    if(idx == -1) {
-//                        idx = 0;
-//                        break;
-//                    }
-//                    cnt++; // 열외 병사
-//                }
-//
-//                answer += cnt;
-//                //System.out.println("answer" +  answer);
-//                d[i] = d[idx] + power[i];
-//                //System.out.println(d[i]);
-//            }
-//        }
-//
-//        System.out.println(answer);
     }
 }
 /*
