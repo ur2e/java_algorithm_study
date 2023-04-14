6bef4a0a8552cf9737f7debfcf3a9a7dd468bbad
Yuri JI <45191069+ur2e@users.noreply.github.com>
2023-04-14 10:25:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,71 +4,63 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Main {
     static int[] dx = {0, 1, 0, -1};
-    static int[] dy= {1, 0, -1, 0};
+    static int[] dy = {-1, 0, 1, 0};
+    static int[][] map, visited;
     static int n;
-    static int[][] map;
 
-    static class Pos {
-        int x, y;
-        Pos(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+    private static int bfs(int x, int y) {
 
-    static int dfs(Pos pos){
-        int sum = 1;
-        Queue<Pos> q = new LinkedList<>();
-        q.offer(pos);
-        map[pos.x][pos.y] = 0;
-        
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        map[x][y] = 0;
+        int cnt = 1;
+
         while(!q.isEmpty()){
-            Pos c = q.poll();
+            int[] curr = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = c.x + dx[i];
-                int ny = c.y + dy[i];
-
-                if(nx >= 0 && nx < n && ny >= 0 && ny < n && map[nx][ny] == 1){
+                int nx = curr[0] + dx[i];
+                int ny = curr[1] + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && map[nx][ny] == 1) {
+                    cnt++;
                     map[nx][ny] = 0;
-                    q.offer(new Pos(nx, ny));
-                    sum++;
+                    q.offer(new int[]{nx, ny});
                 }
             }
         }
-        return sum;
+
+        return cnt;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        int cnt = 0;
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            String[] in = br.readLine().split("");
-            for(int j = 0; j < n; j++){
-                map[i][j] = Integer.parseInt(in[j]);
+
+        map =  new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
         }
 
+        ArrayList<Integer> answer = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(map[i][j] == 1){
-                    ans.add(dfs(new Pos(i, j)));
-                    cnt++;
+                if(map[i][j] == 1) {
+                    answer.add(bfs(i, j));
                 }
             }
         }
 
-        Collections.sort(ans);
-
-        System.out.println(cnt);
-        for(int x: ans){
+        Collections.sort(answer);
+        System.out.println(answer.size());
+        for(int x: answer){
             System.out.println(x);
         }
     }
 }
-
