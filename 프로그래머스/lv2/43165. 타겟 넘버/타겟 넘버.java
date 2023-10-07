61c96ffe031b3cc5512e1bf98eaed06f10259ea2
Yuri JI <45191069+ur2e@users.noreply.github.com>
2023-10-07 19:32:24 +0900

    [level 2] Title: 타겟 넘버, Time: 0.39 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,16 @@
-class Solution {
-    int len = 0;
-    int answer = 0;
-    
-    void dfs(int L, int sum, int target, int[] numbers) {
-        if(L == len){
-            if(sum == target){
-                answer++;
+class Solution {   
+    int dfs(int L, int sum, int target, int[] numbers) {
+        if(L == numbers.length) {
+            if(sum == target) {
+                return 1;
             }
-        } else{
-            dfs(L+1, sum-numbers[L], target, numbers);
-            dfs(L+1, sum+numbers[L], target, numbers);
+            return 0;
         }
+        return dfs(L+1, sum + numbers[L], target, numbers) + dfs(L+1, sum - numbers[L], target, numbers);
     }
     
     public int solution(int[] numbers, int target) {
-        len = numbers.length;
-        dfs(0, 0, target, numbers);
-        return answer;
+        return dfs(0, 0, target, numbers);
+        
     }
 }
