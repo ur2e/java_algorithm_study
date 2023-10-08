4b57076bb7108192dc17ec897ce4b21441abb78c
Yuri JI <45191069+ur2e@users.noreply.github.com>
2023-10-08 12:33:44 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,52 +1,31 @@
 import java.util.*;
 
 class Solution {
-    int[] arr;
-    private int find(int x){
-        if (arr[x] == x) return x;
-        else 
-            return arr[x] = find(arr[x]);
-    }
+    boolean[] visited;
     
-    private void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if(x < y) {
-            arr[y] = x;
-        }
-        else {
-            arr[x] = y;
+    private void dfs(int x, int[][] computers){
+        visited[x] = true;
+        
+        for(int i = 0; i < computers.length; i++){
+            if(!visited[i] && computers[x][i] == 1){
+                dfs(i, computers);
+            }
         }
     }
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        arr = new int[n];
-            
-        for(int i = 0; i < n; i++){
-            arr[i] = i;
-        }
+        visited = new boolean[n];
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(i == j)  continue;
-                if (computers[i][j] == 1) {
-                    union(i,j);
+                if (!visited[i] && computers[i][j] == 1) {
+                    dfs(i, computers);
+                    answer++;
                 }
             }
         }
         
-        Set<Integer> parent = new HashSet<>();
-        parent.add(arr[0]);
-        
-        for(int i = 1; i < n; i++){
-           find(i);
-        }
-        
-        for(int i = 1; i < n; i++){
-            parent.add(arr[i]);
-        }
-        
-        return parent.size();
+        return answer;
     }
 }
