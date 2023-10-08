a1b4247018b1ab6652d52850c2b68d2678b9da39
Yuri JI <45191069+ur2e@users.noreply.github.com>
2023-10-08 13:06:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.07 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,35 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-    
-        int[][] dist = new int[n][m];
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-        
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.offer(new int[]{0,0});
-        int L = 1;
-        while(!q.isEmpty()){
-            L++;
-            int len = q.size();
-            for(int i = 0; i < len; i++){
-                int[] c = q.poll();
-                for(int k = 0; k < 4; k++){
-                    int nx = c[0] + dx[k];
-                    int ny = c[1] + dy[k];
-                    if(nx >= 0 && nx < n && ny >=0 && ny < m && maps[nx][ny] == 1){
-                        dist[nx][ny] = L;
-                        maps[nx][ny] = 0;
-                        q.offer(new int[]{nx,ny});
-                    }
-                }
-            }
-        }
-        if (dist[n-1][m-1] == 0)    return -1;
-        else return dist[n-1][m-1];
+        int answer = 0;
+        return answer;
     }
 }
